16cc6ca16cd017ce1dadedc5e8fd3b662fb0bd96
huynd2174 <22024528@vnu.edu.vn>
2023-10-22 23:08:54 +0700

    the 0.1 version (command line)

---
+++ src/Dictionary/DictionaryCommandline.java
@@ -0,0 +1,36 @@
+package src.Dictionary;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DictionaryCommandline {
+    private Dictionary myDictionary;
+
+    public DictionaryCommandline(Dictionary dictionary) {
+        this.myDictionary = dictionary;
+    }
+
+    public void showAllWords() {
+        List<Word> wordsList = new ArrayList<>(myDictionary.Words);
+        Collections.sort(wordsList, (word1, word2) -> word1.getWord_target().compareToIgnoreCase(word2.getWord_target()));
+
+        System.out.println("No | English | Vietnamese");
+        for (int i = 0; i < wordsList.size(); i++) {
+            Word word = wordsList.get(i);
+            System.out.printf("%d | %s | %s%n", i + 1, word.getWord_target(), word.getWord_explain());
+        }
+    }
+
+    public void dictionaryBasic() {
+        DictionaryManagement dictionaryManagement = new DictionaryManagement(myDictionary);
+        dictionaryManagement.insertFromCommandLine();
+        showAllWords();
+    }
+    public static void main(String[] args){
+        Dictionary eng = new Dictionary();
+        DictionaryCommandline test = new DictionaryCommandline(eng);
+        test.dictionaryBasic();
+    }
+
+}
--- src/Dictionary/DictionaryManagement.java
+++ src/Dictionary/DictionaryManagement.java
@@ -5,8 +5,25 @@ import java.util.Scanner;
 public class DictionaryManagement {
     private Dictionary myDictionary = new Dictionary();
 
-    public void insertFromCommandLine() {
-        Scanner scanner = new Scanner(System.in);
+    public DictionaryManagement(Dictionary myDictionary) {
+        this.myDictionary = myDictionary;
     }
 
+    public void insertFromCommandLine() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of words to insert: ");
+        int numWords = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numWords; i++) {
+            System.out.print("Enter a word in English: ");
+            String word_target = scanner.nextLine();
+
+            System.out.print("Enter the meaning in Vietnamese: ");
+            String word_explain = scanner.nextLine();
+
+            myDictionary.insertWord(word_target, word_explain);
+        }
+    }
 }
