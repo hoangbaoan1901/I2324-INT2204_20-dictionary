9029fe054b21e50ad878668e034a5f82dc10a93f
huynd2174 <22024528@vnu.edu.vn>
2023-11-22 21:12:45 +0700

    2.0

--- src/main/java/Application/Start.java
+++ src/main/java/Application/Start.java
@@ -1,77 +1,48 @@
 package Application;
-import Dictionary.Dictionary;
-import Dictionary.DictionaryCommandline;
-import Dictionary.*;
-import java.util.Scanner;
-import static Dictionary.DictionaryCommandline.printMenu;
 
-public class Start {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Start extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Dictionary eng = new Dictionary();
-        DictionaryCommandline app = new DictionaryCommandline(eng);
+        launch(args);
+    }
 
-        System.out.println("Welcome to My Application!");
-        while (true) {
-            printMenu();
-            System.out.print("Your action: ");
+    @Override
+    public void start(final Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Fxmls/DictionaryGui.fxml"));
 
-            if (scanner.hasNextLine()) {
-                String userInput = scanner.nextLine();
-
-                if (userInput.matches("[0-9]")) {
-                    int action = Integer.parseInt(userInput);
-
-                    switch (action) {
-                        case 0:
-                            System.out.println("Exiting application. Goodbye!");
-                            System.exit(0);
-                            break;
-                        case 1:
-                            app.dictionaryBasic();
-                            break;
-                        case 2:
-                            app.deleteWordFromCommandLine();
-                            break;
-                        case 3:
-                            app.updateWordFromCommandLine();
-                            break;
-                        case 4:
-                            app.showAllWords();
-                            break;
-                        case 5:
-                            app.dictionaryLookup();
-                            break;
-                        case 6:
-                            System.out.print("Enter a prefix to search: ");
-                            String prefix = scanner.nextLine();
-                            app.dictionarySearcher(prefix);
-                            break;
-                        case 7:
-                            System.out.println("Accessing the Game section (not implemented).");
-                            break;
-                        case 8:
-                            System.out.print("Enter the file path to import from: ");
-                            String importFilePath = scanner.nextLine();
-                            app.insertFromFile(importFilePath);
-                            break;
-                        case 9:
-                            System.out.print("Enter the file path to export to: ");
-                            String exportFilePath = scanner.nextLine();
-                            app.dictionaryExportToFile(exportFilePath);
-                            break;
-                        default:
-                            System.out.println("Action not supported");
-                            break;
-                    }
-                } else {
-                    System.out.println("Action not supported");
-                }
-            } else {
-                System.out.println("No input found.");
-                break;
+        stage.setTitle("Dictionary");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
             }
-        }
-        scanner.close();
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.show();
     }
 }
