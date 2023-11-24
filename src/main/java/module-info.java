143837abf3d6d34fe8c2cce2dbad3ab3f3442fae
huynd2174 <22024528@vnu.edu.vn>
2023-11-25 00:28:32 +0700

    2.10

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module Application {
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.media;
     requires java.net.http;
     requires java.sql;
     requires json.path;
@@ -8,6 +9,7 @@ module Application {
     requires org.xerial.sqlitejdbc;
     requires org.json;
     requires freetts;
+    requires java.desktop;
     opens Application to javafx.graphics;
     exports Application;
     exports DictionaryApplication.Controllers;
