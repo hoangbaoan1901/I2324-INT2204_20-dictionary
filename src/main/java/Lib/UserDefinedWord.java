54428c4779751a2fbc4b2df87a7d10221a447e18
hoangbaoan1901 <hoangbaoan1901@gmail.com>
2023-11-24 23:14:55 +0700

    Remove unnecessary files, adding phrasal_verbs to database.

--- src/main/java/Lib/UserDefinedWord.java
+++ src/main/java/Lib/UserDefinedWord.java
@@ -1,5 +1,43 @@
 package Lib;
 
-public class UserDefinedWord {
+import java.sql.Connection;
+import java.sql.Statement;
 
+public class UserDefinedWord {
+    private String word;
+    private String meaning;
+
+    public UserDefinedWord(String word, String meaning) {
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public static void addWordToDatabase(UserDefinedWord word) {
+        String key = word.getWord();
+        String definition = word.getMeaning();
+        Connection c = SQLiteConnect.getSQLiteConnection("src/main/resources/Databases/Dictionary.db");
+        Statement statement = null;
+        try {
+            statement = c.createStatement();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/Lib/WordCreate.java
+++ src/main/java/Lib/WordCreate.java
@@ -6,11 +6,3 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class WordCreate {
-    public static void addWordToDatabase(Word w) {
-        String word = w.getWord();
-        HashMap<String, ArrayList<Pair<String, String>>> meanings = w.getMeanings();
-        Connection c = SQLiteConnect.getSQLiteConnection("src/main/resources/Databases/Dictionary.db");
-
-    }
-}
